c74bcf1aeb6b7459f4695f0505b9d100d0db8b31
hersson ceballos <herssonceballos@gmail.com>
2020-08-07 20:35:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,6 +3,7 @@ def roll_call_dwarves(array)
   array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
 end
 
+
 def summon_captain_planet(array)
   planeteer_calls = []
   planeteer_calls << array.collect { |calls| "#{calls.capitalize}!"}
