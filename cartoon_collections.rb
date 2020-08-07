a024c6ce805dbd9f3473d4ead0cba7e1df025b51
hersson ceballos <herssonceballos@gmail.com>
2020-08-07 20:43:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,12 +13,10 @@ end
 
 
 
-
-
-
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls()
 end
+
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
