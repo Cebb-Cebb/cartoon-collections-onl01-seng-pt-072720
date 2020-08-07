8e2c6e94726c7bba15b8aa370654cb42d8752424
hersson ceballos <herssonceballos@gmail.com>
2020-08-07 19:25:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,4 @@
 def roll_call_dwarves(array)
-  i = 0 
-  while i < array.length 
-    puts "#{i + 1}. #{array[i]}"
-    i += 1 
-  end 
-end
-
-def roll_call_dwarves(array)
   i = 0
   array.each_with_index {|name, index| puts "#{i + 1}. #{name} "}
 end
